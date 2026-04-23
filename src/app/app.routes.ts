3f57d7f97dadc5837a9e228747ef4b7903df4dab
manojkumarsharma90 <mukul2002sha14@gmail.com>
2026-04-23 15:15:17 +0530

    first init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RecordtableComponent } from './recordtable/recordtable.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'bank',component:RecordtableComponent}
+];
